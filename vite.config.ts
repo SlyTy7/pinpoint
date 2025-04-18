04bcea09212a21c61359eff62ca81b510adbb1ca
SlyTy7 <tyler.west.dev@gmail.com>
2025-04-18 12:52:11 -0700

    add rollup-plugin-copy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import copy from 'rollup-plugin-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "/",
+  plugins: [
+    react(),
+    copy({
+      targets: [
+        { src: 'dist/index.html', dest: 'dist', rename: '404.html' }
+      ],
+      hook: 'writeBundle'
+    })
+  ],
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
 })
